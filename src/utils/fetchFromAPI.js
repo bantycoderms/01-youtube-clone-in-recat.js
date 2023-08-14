d0d0d28f6d8f962549d3ffe6eb773aece311dc61
bantycoderms <124484621+bantycoderms@users.noreply.github.com>
2023-08-14 15:17:28 +0530

    first commit

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    "X-RapidAPI-Key": "e278601e5bmsh5122a9c077192d7p1723bejsndcc1e2800450",
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
